4ceaed46d12753363890995847a395375df797ee
Gopika-Kasilingam <gopi22038.cs@rmkec.ac.in>
2025-07-22 20:33:11 +0530

    Add files

---
+++ Mandatory tasks/React2-HOL/studentapp/src/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About Student App</h1>
+      <p>This application is built using React.</p>
+    </div>
+  );
+}
+
+export default About;
